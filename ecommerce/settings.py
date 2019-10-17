038085040748b369d5250555c801d062c2dcb44a
Dave Laffan <dave.laffan@outlook.com>
2019-10-17 16:21:55 +0100

    Add search app & basic styling

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -128,15 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, 'static'),
+    )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
-
-
-# Below added to fix / enable static files
-STATICFILES_DIRS = (
-     os.path.join(BASE_DIR, 'static'),
-)
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
