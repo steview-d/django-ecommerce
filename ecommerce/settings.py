9452603fb95e5a332a4c166f7443c3df2860cf33
Dave Laffan <dave.laffan@outlook.com>
2020-01-21 17:17:29 +0000

    Test....

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -150,6 +150,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# STATICFILES_LOCATION = 'static'
+# for whitenoise
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticf')
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, 'static'),
+    )
+
+# AWS S3
 AWS_S3_OBJECT_PARAMETERS = {
     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
     'CacheControl': 'max-age=94608000',
@@ -159,21 +170,9 @@ AWS_STORAGE_BUCKET_NAME = 'django-shop-sw'
 AWS_S3_REGION_NAME = 'eu-west-2'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-# STATICFILES_LOCATION = 'static'
-
-
-STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#      os.path.join(BASE_DIR, 'static'),
-#     )
-
-# for whitenoise
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+# Media Files
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
